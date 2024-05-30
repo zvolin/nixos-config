75fde4a464ecc95437c37826f7921fb2cb83b66f
zvolin <mac.zwolinski@gmail.com>
2024-05-31 00:03:43 +0200

    feat(nixvim): add winbar with nvim-navic

--- nix/nixvim/plugins/lualine.nix
+++ nix/nixvim/plugins/lualine.nix
@@ -1,18 +1,90 @@
-{ ... }:
+{ pkgs, lib, ... }:
 
 {
-  programs.nixvim.plugins.lualine = {
-    enable = true;
+  programs.nixvim = {
+    plugins.lualine = {
+      enable = true;
 
-    disabledFiletypes.statusline = [ "neo-tree" "undotree" ];
+      # have single statusline instead per-window
+      globalstatus = true;
+      # exclude side panels
+      disabledFiletypes.statusline = [ "neo-tree" "undotree" ];
 
-    componentSeparators = {
-      left = "";
-      right = "";
+      componentSeparators = {
+        left = "";
+        right = "";
+      };
+      sectionSeparators = {
+        left = "";
+        right = "";
+      };
+
+      # add navic to the top winbar
+      winbar.lualine_c = [
+        {
+          name = "navic";
+          # always set hl group to have uniform line color, even if navic has no output
+          # offset the output by the width of number column
+          fmt = ''function(text) return "%#NavicText#     " .. text end'';
+        }
+      ];
+      inactiveWinbar.lualine_c = [
+        {
+          name = "navic";
+          fmt = ''function(text) return "%#NavicText#" end'';
+        }
+      ];
     };
-    sectionSeparators = {
-      left = "";
-      right = "";
-    };
+
+    extraPlugins = with pkgs.vimPlugins; [
+      nvim-navic
+    ];
+
+    extraConfigLua = let
+      # set the hl groups for navic items. maybe upstream to 'base16-nvim'?
+      hl_group_map = {
+        NavicIconsFile = "Directory";
+        NavicIconsModule = "@module";
+        NavicIconsNamespace = "@module";
+        NavicIconsPackage = "@module";
+        NavicIconsClass = "Type";
+        NavicIconsMethod = "@function.method";
+        NavicIconsProperty = "@property";
+        NavicIconsField = "@variable.member";
+        NavicIconsConstructor = "@constructor";
+        NavicIconsEnum = "Type";
+        NavicIconsInterface = "Type";
+        NavicIconsFunction = "Function";
+        NavicIconsVariable = "@variable";
+        NavicIconsConstant = "Constant";
+        NavicIconsString = "String";
+        NavicIconsNumber = "Number";
+        NavicIconsBoolean = "Boolean";
+        NavicIconsArray = "Type";
+        NavicIconsObject = "Type";
+        NavicIconsKey = "Identifier";
+        NavicIconsNull = "Type";
+        NavicIconsEnumMember = "Constant";
+        NavicIconsStruct = "Structure";
+        NavicIconsEvent = "Structure";
+        NavicIconsOperator = "Operator";
+        NavicIconsTypeParameter = "Type";
+        NavicText = "Comment";
+        NavicSeparator = "Comment";
+      };
+    in ''
+      require("nvim-navic").setup({
+        highlight = true,
+        lsp = {
+          auto_attach = true,
+        }
+      })
+    '' +
+    lib.concatStringsSep "\n" (
+      lib.attrsets.mapAttrsToList (
+        hl_group: source_group: ''vim.api.nvim_set_hl(0, "${hl_group}", { link = "${source_group}" })''
+      )
+      hl_group_map
+    );
   };
 }
