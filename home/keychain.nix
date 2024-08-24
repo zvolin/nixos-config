9b4d6944543ffd1672a5dc40797324178f0ab8fe
zvolin <mac.zwolinski@gmail.com>
2024-08-24 15:34:20 +0200

    feat: remove keychain and use agents

--- home/keychain.nix
+++ home/keychain.nix
@@ -1,17 +1,18 @@
 { pkgs, ... }:
 
 {
-  pam.sessionVariables = {
-    SSH_AGENT_PID = "DEFAULT=";
-    SSH_AUTH_SOCK = "DEFAULT=\${XDG_RUNTIME_DIR}/gnupg/S.gpg-agent.ssh";
+  programs.ssh = {
+    enable = true;
+    addKeysToAgent = "yes";
   };
 
+  services.ssh-agent.enable = true;
+
   services.gpg-agent = {
+    enable = true;
+
     defaultCacheTtl = 84000;
     maxCacheTtl = 84000;
-
-    defaultCacheTtlSsh = 84000;
-    maxCacheTtlSsh = 84000;
 
     pinentryPackage = pkgs.pinentry-curses;
   };
