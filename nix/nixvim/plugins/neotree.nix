01eea695d90bc9a84f31da111b51344670a6f246
zvolin <mac.zwolinski@gmail.com>
2026-04-18 00:37:09 +0200

    feat(neotree): add yy/Y keybindings for path clipboard yank

--- nix/nixvim/plugins/neotree.nix
+++ nix/nixvim/plugins/neotree.nix
@@ -7,6 +7,26 @@
       enable = true;
 
       settings = {
+        commands = {
+          copy_relative_path.__raw = ''
+            function(state)
+              local node = state.tree:get_node()
+              if not node or not node.path then return end
+              local path = vim.fn.fnamemodify(node.path, ":.")
+              vim.fn.setreg("+", path)
+              vim.notify("Copied: " .. path)
+            end
+          '';
+          copy_absolute_path.__raw = ''
+            function(state)
+              local node = state.tree:get_node()
+              if not node or not node.path then return end
+              vim.fn.setreg("+", node.path)
+              vim.notify("Copied: " .. node.path)
+            end
+          '';
+        };
+
         filesystem = {
           follow_current_file = {
             enabled = true;
@@ -24,6 +44,9 @@
           mappings = {
             # overwrite default open with window picker
             "<cr>" = "open_with_window_picker";
+            "y" = "noop"; # disable default file-copy to allow yy prefix
+            "yy" = "copy_relative_path";
+            "Y" = "copy_absolute_path";
           };
         };
 
