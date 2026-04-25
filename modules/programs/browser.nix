8973c9cd3892c6cedc9ba6ce790d98e5d821ab2c
zvolin <mac.zwolinski@gmail.com>
2026-04-26 01:13:53 +0200

    fix(flake): update all inputs, adopt Firefox XDG config path

--- modules/programs/browser.nix
+++ modules/programs/browser.nix
@@ -1,13 +1,16 @@
-{ ... }:
-
-{
-  flake.modules.homeManager.browser = { pkgs, ... }: {
+{...}: {
+  flake.modules.homeManager.browser = {
+    pkgs,
+    config,
+    ...
+  }: {
     # https://discourse.nixos.org/t/declare-firefox-extensions-and-settings/36265
     programs.firefox = {
       enable = true;
+      configPath = "${config.xdg.configHome}/mozilla/firefox";
     };
 
-    stylix.targets.firefox.profileNames = [ "default" ];
+    stylix.targets.firefox.profileNames = ["default"];
 
     programs.chromium = {
       enable = true;
