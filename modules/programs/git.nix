85ee76647fe7704b7c6937c3a97dc5702a85a356
zvolin <mac.zwolinski@gmail.com>
2026-04-30 08:41:28 +0000

    feat(git): set user name and email

--- modules/programs/git.nix
+++ modules/programs/git.nix
@@ -3,6 +3,11 @@
     programs.git = {
       enable = true;
 
+      settings.user = {
+        name = "zvolin";
+        email = "mac.zwolinski@gmail.com";
+      };
+
       ignores = [
         "**/.claude/worktrees/"
         "**/.claude/settings.local.json"
